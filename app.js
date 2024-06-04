024c9c307e46ed3d2b3d796db5175106a5a8772c
ikaronauta <81399721+ikaronauta@users.noreply.github.com>
2024-06-04 15:36:53 -0500

    se agrega logica para almacenar los datos en locastorage

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
-import { getFirestore, getDocs, collection, query, where, addDoc  } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
+import {
+  getFirestore,
+  getDocs,
+  collection,
+  query,
+  where,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
 import { irregulars } from "./data.js";
+
+let date;
+let dataVerbs = [];
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -11,7 +21,7 @@ const firebaseConfig = {
   storageBucket: "irregular-verbs-b8dc9.appspot.com",
   messagingSenderId: "597971243862",
   appId: "1:597971243862:web:c2bd34a95e4e50e6f97cf7",
-  measurementId: "G-94DFHYC6MD"
+  measurementId: "G-94DFHYC6MD",
 };
 
 // Initialize Firebase
@@ -19,7 +29,10 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-
+function inicio() {
+  date = new Date().toLocaleDateString().replaceAll("/", "-");
+  validateLocalStorage();
+}
 
 document.getElementById("send").addEventListener("click", function () {
   let verb = document.getElementById("verb").value;
@@ -28,16 +41,36 @@ document.getElementById("send").addEventListener("click", function () {
   //sendData();
 });
 
+function validateLocalStorage() {
+  if (localStorage.getItem(`${date}-data`)) {
+    dataVerbs = JSON.parse(localStorage.getItem(`${date}-data`));
+    return;
+  }
+
+  getAllDocs();
+}
+
 async function getAllDocs() {
   const querySnapshot = await getDocs(collection(db, "irregulars"));
+
+  querySnapshot.forEach((doc) => {
+    dataVerbs.push({
+      id: doc.id,
+      data: doc.data(),
+    });
+  });
+
+  console.log(dataVerbs);
+
+  localStorage.setItem(`${date}-data`, JSON.stringify(dataVerbs));
+  return;
   querySnapshot.forEach((doc) => {
     console.log(`${doc.id} => ${doc.data().base}`);
   });
 }
 
-async function getDocForEs(verb){
-
-  if(verb == '') {
+async function getDocForEs(verb) {
+  if (verb == "") {
     alert("Debe ingresar un verbo");
     return;
   }
@@ -45,15 +78,18 @@ async function getDocForEs(verb){
   console.log(`Consultando ${verb}...`);
 
   const irregularsCollection = collection(db, "irregulars");
-  const q = query(irregularsCollection, where("es", "array-contains", verb.toLowerCase()));
+  const q = query(
+    irregularsCollection,
+    where("es", "array-contains", verb.toLowerCase())
+  );
 
   const querySnapshot = await getDocs(q);
 
-  if(querySnapshot.size > 0){
+  if (querySnapshot.size > 0) {
     querySnapshot.forEach((doc) => {
       console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
     });
-  } else{
+  } else {
     console.log(`No se encontro ${verb}`);
   }
 }
@@ -61,17 +97,21 @@ async function getDocForEs(verb){
 let count = 0;
 
 async function sendData() {
+  console.log("enviando");
 
-    console.log("enviando");
+  try {
+    const docRef = await addDoc(
+      collection(db, "irregulars"),
+      irregulars[count]
+    );
+    console.log("Document written with ID: ", docRef.id);
+    count++;
 
-    try {
-        const docRef = await addDoc(collection(db, "irregulars"), irregulars[count]);
-        console.log("Document written with ID: ", docRef.id);
-        count++;
-
-        if (count <= irregulars.length) sendData();
-        else console.log("Guardado Finalizado");
-    } catch (e) {
-        console.error("Error adding document: ", e);
-    }
+    if (count <= irregulars.length) sendData();
+    else console.log("Guardado Finalizado");
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 }
+
+inicio();
